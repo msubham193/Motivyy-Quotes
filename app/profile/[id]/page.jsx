7e1c92faad99123a818434fa6f735515d3e7d0b7
Subham Mishra <subhammishr@Subhams-MacBook-Air.local>
2023-05-07 09:34:55 +0530

    done

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -2,8 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import Profile from "@/components/Profile";
 
-import Profile from "@components/Profile";
+
 
 const UserProfile = ({ params }) => {
   const searchParams = useSearchParams();
